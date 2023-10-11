277bf37e03610183272f89e1d8c2b2cb6700704d
farooq <faroooq9207@gmail.com>
2023-10-11 20:49:16 +0530

    adminn

--- src/Routes/Admin.jsx
+++ src/Routes/Admin.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route,Navigate} from "react-router-dom";
 import Service from "../Pages/Admin/AdminService"
 import User from "../Pages/Admin/User" 
-import Login from "../Components/BaseLogin"
 import Provider from "../Pages/Admin/Provider";
 import { useSelector } from "react-redux";
 import OrderPage from "../Pages/Admin/OrderPage";
 import SingleOrderPage from "../Pages/Admin/SingleOrderPage";
 import HomePage from "../Pages/Admin/HomePage";
+import BaseLogin from "../Components/BaseLogin";
 
 
 const Admin = () => {
@@ -16,7 +16,7 @@ const Admin = () => {
     <Routes>
         <Route path="/order"  element={adminAuth ? <SingleOrderPage/> : <Navigate to='/admin/login'/>}/>
         <Route path="/orders"  element={adminAuth ? <OrderPage/> : <Navigate to='/admin/login'/>}/>
-        <Route path="/login" element={adminAuth ? <Navigate to='/admin' /> : <Login role='admin'  /> }/>
+        <Route path="/login" element={adminAuth ? <Navigate to='/admin' /> : <BaseLogin role='admin'  /> }/>
         <Route path="/home" element={adminAuth ? <HomePage/> : <Navigate to='/admin/login' />  }/>
         <Route path="/services" element={adminAuth ? <Service/> : <Navigate to='/admin/login' />  }/>
         <Route path="/users" element={adminAuth ? <User/> : <Navigate to='/admin/login' />  }/>
