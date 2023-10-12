5f19aa9170910b56e8b0ae2d6bc9b7be4d92d740
farooq <faroooq9207@gmail.com>
2023-10-12 16:43:36 +0530

    login

--- src/Routes/User.jsx
+++ src/Routes/User.jsx
@@ -1,7 +1,6 @@
 // import React from 'react'
 import { Routes, Route,Navigate} from "react-router-dom";
 import { useSelector } from "react-redux";
-import OrderPage from "../Pages/User/OrderPage";
 import SingleOrderPage from "../Pages/User/SingleOrderPage";
 import ChatPage from "../Pages/User/ChatPage";
 import SingleProviderPage from "../Pages/User/SingleProviderPage";
@@ -13,6 +12,7 @@ import ProvidersPage from "../Pages/User/ProviderPage";
 import HomePage from "../Pages/User/HomePage";
 import PyamentPage from "../Pages/User/PyamentPage";
 import CheckoutPage from "../Pages/User/CheckOut";
+import OrderPage from "../Pages/User/OrderPage";
 
 
 
